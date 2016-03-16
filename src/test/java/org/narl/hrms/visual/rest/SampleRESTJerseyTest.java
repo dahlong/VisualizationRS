9c58d2a076624ca1f166540dfb4bdf5b0c17abbf
dahlong <dahlong@gmail.com>
2016-03-16 17:51:01 +0800

    pom.xml 檔增加 jetty 的 context path and Comment <executions> tag. 否則 Eclpse 上 run on Tomcat server 與 mvn clean package jetty:deploy-war 會有錯誤訊息

--- src/test/java/org/narl/hrms/visual/rest/SampleRESTJerseyTest.java
+++ src/test/java/org/narl/hrms/visual/rest/SampleRESTJerseyTest.java
@@ -1,19 +1,10 @@
 package org.narl.hrms.visual.rest;
 
 import static org.junit.Assert.*;
-
 import javax.ws.rs.core.Application;
-
 import org.glassfish.jersey.server.ResourceConfig;
-import org.glassfish.jersey.server.spring.SpringLifecycleListener;
-import org.glassfish.jersey.server.spring.scope.RequestContextFilter;
-import org.glassfish.jersey.servlet.ServletContainer;
-import org.glassfish.jersey.test.DeploymentContext;
 import org.glassfish.jersey.test.JerseyTest;
-import org.glassfish.jersey.test.ServletDeploymentContext;
 import org.junit.Test;
-import org.narl.hrms.visual.ApplicationConfig;
-import org.springframework.web.context.ContextLoaderListener;
 
 public class SampleRESTJerseyTest extends JerseyTest {
 
